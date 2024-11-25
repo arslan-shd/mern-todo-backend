2bada9ef94db5a84e5a29e30a221738122a6db70
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-11-25 10:56:37 +0530

    fixing some module not found errors

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const path = require("path");
 const todosRouter = require("./routes/todoRoutes");
 const usersRouter = require("./routes/userRoutes");
-const subscriptionsRouter = require("./routes/subscriptionRoutes");
 const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
@@ -20,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/todos", todosRouter);
 app.use("/api/v1/users", usersRouter);
-app.use("/api/v1/subscribe", subscriptionsRouter);
+
 app.use(globalErrorHandler);
 
 module.exports = app;
