7fa08e1f0b4d3967da3c099677e6e9b1b7499726
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-12-03 20:51:03 +0530

    changed url

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: process.env.CLIENT_URL,
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
+    // origin: "http://localhost:5173",
   })
 );
 
